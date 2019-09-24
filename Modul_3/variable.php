3eb53096ed648dfee10a9d09d7df69309943ad11
L200170018 <34600649+L200170018@users.noreply.github.com>
2019-09-24 13:32:00 +0700

    Add files via upload

--- Modul_3/variable.php
+++ Modul_3/variable.php
@@ -1,36 +1,27 @@
 <html>
-    <head>
-        <title>variabel</title>
-    </head>
-    <body>
-        <h1>Nilai</h1>
-        <form method ='POST' action='kendali.php'>
-            <p>Masukan nilai angka (0-100) : <input type='text' name='nilai' size='3'></p>
-            <p><input type='submit' value='proses' name='submit'></p>
-        </form>
-        <?php
-        error_reporting (E_ALL ^ E_NOTICE);
-        $nilai = $_POST ['nilai'];
-        $submit = $_POST['submit'];
-        if($submit){
-        if($nilai==''){
-        $huruf= '"Nilai kosong/belum diisi"';
-        }elseif($nilai<=20){
-        $huruf='E';
-        }elseif($nilai<=40){
-        $huruf='D';
-        }elseif($nilai<=60){
-        $huruf='C';
-        }elseif($nilai<=80){
-        $huruf='B';
-        }elseif($nilai<=100){
-        $huruf='A';
-        }else{
-        $huruf='Nilai yang dimasukan salah!"';
-        }
-        echo"Nilai angka adalah $nilai</br>";
-        echo"Maka nilai huruf adalah $huruf";
-        }
-        ?>
-    </body>
- </html>
+<head>
+<title>variable</title>
+</head>
+<body>
+<h1>Buku Tamu</h1>
+<form method='POST' action='variable.php'>
+<p>Nama : <input type='text' name='nama' size='20'></p>
+<p>Email : <input type='text' name='email' size='30'></p>
+<p>Komentar : <textarea name='komentar' cols='30'
+rows='3'></textarea></p>
+<p><input type='submit' value='kirim' name='submit'></p>
+</form>
+<?php
+error_reporting (E_ALL ^ E_NOTICE);
+$nama = @_POST['nama'];
+$email = @_POST['email'];
+$komentar = @_POST['komentar'];
+$submit = $_POST['submit'];
+if($submit) {
+echo"</br>nama : $nama";
+echo"</br>email : $email";
+echo"</br>komentar : $komentar";
+}
+?>
+</body>
+</html> 
